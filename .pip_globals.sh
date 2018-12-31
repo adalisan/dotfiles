7106ea745a4fcffff1cc40b17184268ef26c8331
Sancar Adali <sadali+git@gmail.com>
2018-12-31 05:15:27 -0500

    pip globals  merge fix

--- .pip_globals.sh
+++ .pip_globals.sh
@@ -7,13 +7,8 @@ function pip_install_globals() {
 
 # Start the actual installation of the recipes
 globals=(
-<<<<<<< HEAD
   pandas
   networkx
-=======
-pandas
-networkx
->>>>>>> 63a1fc4... try to fix prompt. add-remove pip installs
 )
 
 # Start the actual installation of the recipes
