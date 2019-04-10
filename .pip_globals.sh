006420c89379f1b4b3b01a187d1e54135554c86a
Sancar Adali <sadali+git@gmail.com>
2019-04-10 12:04:03 -0500

    pip install to user if non-root

--- .pip_globals.sh
+++ .pip_globals.sh
@@ -1,7 +1,11 @@
 # Install PIP globals
 function pip_install_globals() {
   for global in "${globals[@]}"; do
+    if [[ -z "$NOTSUDOER" ]]; then
     sudo pip install $global
+    else
+    pip install --user $global
+    fi 
   done
 }
 
