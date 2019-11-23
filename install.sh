43370661664065b89d63c0923e4ca0fb0cee9fc4
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2019-11-22 21:55:40 -0500

    fix install.sh

--- install.sh
+++ install.sh
@@ -15,17 +15,16 @@ function linkDotFiles() {
 	# Link the files from source to the target with a dot appended to the front.
 	find $SOURCE_LOCATION -mindepth 1 -maxdepth 1 -name '.*'| while read file; do
 		if [[ $(basename $file) != ".git" && $(basename $file) != ".gitignore" ]]; then
-      		if [[ -d ${HOME}/$(basename $file) ]]; then
-      			echo "${NC}$(basename $file)${NC} is a driectory, ${red}removing a directory now ..${NC}"
-      			rm -r "${HOME}/$(basename $file)"
-      		elif [[ -f ${HOME}/$(basename $file) ]]; then
-      			echo "${magenta}$(basename $file)${NC} is found, ${red}deleting and overwriting now!${NC}"
-      			rm "${HOME}/$(basename $file)"
-    		else
-    		  echo "Linking ${magenta}$file${NC} to ${magenta}$TARGET_LOCATION/$(basename $file)${NC}"
-    	       ln -s "$file" "${HOME}/$(basename $file)"
-    		fi
-        fi
+  			if [[ -d ${HOME}/$(basename $file) ]]; then
+  				printf "${NC}$(basename $file)${NC} is a driectory, ${red}removing a directory now ..\n${NC}"
+  				rm -r "${HOME}/$(basename $file)"
+  			else
+  				printf "${magenta}$(basename $file)${NC} is found, ${red}deleting and overwriting now!\n${NC}"
+  				rm "${HOME}/$(basename $file)"
+			fi
+		    printf "Linking ${magenta}$file${NC} to ${magenta}$TARGET_LOCATION/$(basename $file)\n${NC}"
+	    	ln -s "$file" "${HOME}/$(basename $file)"
+		fi
 	done
 }
 
